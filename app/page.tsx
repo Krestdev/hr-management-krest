099d0cb449727573a648ab37fcad77f06c97e25c
Hansjason97 <hansadiogo@gmail.com>
2025-11-12 17:28:51 +0100

    added redirection on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>Page</div>
-  );
+  redirect("/tableau-de-bord");
 }
